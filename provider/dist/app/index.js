068e51d3db79ca1164d809c9e03c5585456d86f3
Rodion <rybol2593@gmail.com>
2021-08-22 19:06:55 +0300

    Remove dist folder

--- provider/dist/app/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.app = void 0;
-var app_1 = require("./app");
-Object.defineProperty(exports, "app", { enumerable: true, get: function () { return __importDefault(app_1).default; } });
--- provider/dist/app/oidc.d.ts
+++
@@ -1,2 +0,0 @@
-declare const oidc: any;
-export default oidc;
